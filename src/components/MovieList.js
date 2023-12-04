06c808bd2aee2348281b8c02b1933368f3f56b26
epgokce <142578266+epgokce@users.noreply.github.com>
2023-12-04 23:47:49 +0300

    Display conditional rendering for an empty movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,29 +5,33 @@ import MovieFooter from "./MovieFooter";
 import FavoriteMovieList from "./FavoriteMovieList";
 
 const MovieList = (props) => {
-  const { movies, favoriteMovies } = props;
+  const { movies } = props;
 
   return (
     <div className="flex-1">
-      <div className="overflow-hidden bg-white rounded-md shadow mb-4 sm:min-h-[400px]">
-        <table className="table-auto border-collapse text-left w-full">
-          <thead>
-            <tr className="border-zinc-200 border-b">
-              <th className="pl-4">İsim</th>
-              <th>Yönetmen</th>
-              <th>Tür</th>
-              <th>Metascore</th>
-              <th></th>
-            </tr>
-          </thead>
+      {movies.length === 0 ? (
+        <p>No movies to display.</p>
+      ) : (
+        <div className="overflow-hidden bg-white rounded-md shadow mb-4 sm:min-h-[400px]">
+          <table className="table-auto border-collapse text-left w-full">
+            <thead>
+              <tr className="border-zinc-200 border-b">
+                <th className="pl-4">İsim</th>
+                <th>Yönetmen</th>
+                <th>Tür</th>
+                <th>Metascore</th>
+                <th></th>
+              </tr>
+            </thead>
 
-          <tbody className="text-sm">
-            {movies.map((movie) => (
-              <MovieListItem key={movie.id} movie={movie} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+            <tbody className="text-sm">
+              {Array.isArray(movies) && movies.length > 0 && movies.map((movie) => (
+                <MovieListItem key={movie.id} movie={movie} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
       <MovieFooter totalMovies={movies.length} />
     </div>
   );
